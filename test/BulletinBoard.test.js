40bfc2d1627501a1537379a46b39df3e22ea3c5f
maxwellpothier <pothiermaxwell@gmail.com>
2022-07-26 20:41:33 -0600

    new directory structure

--- test/BulletinBoard.test.js
+++ test/BulletinBoard.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const {abi, evm} = require("../compile");
+const {abi, evm} = require("../ethereum/compile");
 
 let board;
 let accounts;
