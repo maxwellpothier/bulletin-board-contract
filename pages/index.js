3d7101309a7d6fd6d6b070943038ad6dfb633e90
maxwellpothier <pothiermaxwell@gmail.com>
2022-07-27 23:27:36 -0600

    add message button is functional, without any UI

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import boardInstance from "../ethereum/bulletinBoard";
+import web3 from "../ethereum/web3";
 
 const Home = ({messages}) => {
+	const [newMessage, setNewMessage] = useState("");
+
+	const addMessageToBlockchain = async () => {
+		console.log(newMessage);
+
+		const accounts = await web3.eth.getAccounts();
+
+		await boardInstance.methods.addMessage(newMessage).send({
+			from: accounts[0],
+		});
+	};
 
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<input
+				type={"text"}
+				value={newMessage}
+				onChange={(e) => setNewMessage(e.target.value)}
+			/>
+			<button onClick={addMessageToBlockchain}>Add a message</button>
+
 			{messages.map((message, i) => (
 				<div key={i}>
 					<h3>{message[0]}</h3>
