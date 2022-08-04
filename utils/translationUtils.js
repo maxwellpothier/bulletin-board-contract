84400d3856bec8b2c5a96290cbee91122665946b
maxwellpothier <pothiermaxwell@gmail.com>
2022-08-03 20:08:30 -0600

    sets up basic try catch statements on utils

--- utils/translationUtils.js
+++ utils/translationUtils.js
@@ -3,25 +3,37 @@ import boardInstance from "../ethereum/bulletinBoard";
 
 
 export const addMessageToBlockchain = async (newMessage) => {
-	const accounts = await web3.eth.getAccounts();
-
-	await boardInstance.methods.addMessage(newMessage).send({
-		from: accounts[0],
-	});
+	try {
+		const accounts = await web3.eth.getAccounts();
+	
+		await boardInstance.methods.addMessage(newMessage).send({
+			from: accounts[0],
+		});
+	} catch (err) {
+		console.log("This is the return: ", err);
+	}
 };
 
 export const deleteMessageFromBlockchain = async (index) => {
-	const accounts = await web3.eth.getAccounts();
-
-	await boardInstance.methods.deleteMessage(index).send({
-		from: accounts[0],
-	});
+	try {
+		const accounts = await web3.eth.getAccounts();
+	
+		await boardInstance.methods.deleteMessage(index).send({
+			from: accounts[0],
+		});
+	} catch (err) {
+		console.log("This is the return: ", err);
+	}
 };
 
 export const editMessageOnBlockchain = async (index, editedMessage) => {
-	const accounts = await web3.eth.getAccounts();
-
-	await boardInstance.methods.editMessage(index, editedMessage).send({
-		from: accounts[0],
-	});
+	try {
+		const accounts = await web3.eth.getAccounts();
+	
+		await boardInstance.methods.editMessage(index, editedMessage).send({
+			from: accounts[0],
+		});
+	} catch (err) {
+		console.log("This is the return: ", err);
+	}
 };
